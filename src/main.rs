e700ce3a1a81f98743f7cebd2f6751031612df6a
Njagi Mwaniki <njagi@urbanslug.com>
2019-11-08 16:33:57 +0300

    Figure out slicing of Cow<str> values
    Signed-off-by: Njagi Mwaniki <njagi@urbanslug.com>

--- src/main.rs
+++ src/main.rs
@@ -1,49 +1,24 @@
-#![allow(unused_imports)]
-use needletail::{parse_sequence_path, Sequence, SequenceRecord};
+#![allow(dead_code, unused_imports, unused_variables, unused_imports)]
 use std::borrow::Cow;
-use std::str;
-use vcf::*;
-use flate2::read::MultiGzDecoder;
-use std::fs::File;
-use std::io;
-
-fn process_fasta(x: SequenceRecord) -> () {
-    let y = str::from_utf8(&x.id).unwrap();
-    println!("{}", y);
-    
-}
-
-const VCF_FILE: &str = "/Users/urbanslug/src/racket/graphite/data/RSV/refererence_and_vcf_file/H_3801_22_04.freebayes.vcf";
-const FASTA_FILE: &str = "/Users/urbanslug/src/racket/graphite/data/RSV/refererence_and_vcf_file/9465113.fa";
-//const FASTA_FILE: &str = "/Users/urbanslug/src/racket/graphite/data/test/tiny_fake_ref.fa";
 
 
 
-fn read_vcf() {
-    let p = File::open(VCF_FILE).unwrap();
-    let mut vcf_reader = VCFReader::new(p).unwrap();
-
-    for one in vcf_reader {
-        let record = one.unwrap();
-        // process a record
-        println!("{}",  record.chromosome);
-    }
-}
-
-fn read_fasta() {
-    let filename = FASTA_FILE;
-
-    parse_sequence_path(
-        filename,
-        |_| {},
-        |seq| {
-            process_fasta(seq);
-        },
-    )
-    .expect("parsing failed");
+fn splitter<'a>(seq: &'a Cow<str>) -> &'a str {
+    &seq[0..10]
 }
 
 fn main() {
-    //read_vcf();
-    read_fasta();
+    let seq: String = String::from("\
+ACAACAAACTTGCGTAAACCAAAAAAATGGGGC\
+AAATAAGAATTTGATAAGTACCACTTAAATTTA\
+ACTCCTTTGGTTAGAGATGGGCAGCAACTCATT\
+CCTGGGACACTCTCAATCATCTATTATTCATATC\
+ATCGTGCTTATACAAGTTAAATCTTAAATCTATA\
+GCACAAATCACATTATCTATTTTGGCAATGATAA\
+TCTCAACCTCACGAGTATGATAGCACAAATCAGT\
+");
+    // Cow<str> is a borrow, so it will be &str internally
+    let cow_seq: Cow<str> = Cow::Borrowed(&seq);
+    let first_ten = splitter(&cow_seq);
+    println!("{}", first_ten);
 }
