7f7291ab5cd47812becccdc1971e397596985ee5
Njagi Mwaniki <njagi@urbanslug.com>
2019-11-09 15:24:39 +0300

    Insert a borrowed CoW slice into a HashMap
    Signed-off-by: Njagi Mwaniki <njagi@urbanslug.com>

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,15 @@
 #![allow(dead_code, unused_imports, unused_variables, unused_imports)]
 use std::borrow::Cow;
+use std::collections::HashMap;
 
 
+fn splitter<'a>(seq: &'a Cow<str>, graph: &mut HashMap<u64, &'a str>) -> () {
 
-fn splitter<'a>(seq: &'a Cow<str>) -> &'a str {
-    &seq[0..10]
+
+    let x: &'a str =  &seq[0..10];
+    graph.insert(10, x);
+
+    ()
 }
 
 fn main() {
@@ -12,13 +17,20 @@ fn main() {
 ACAACAAACTTGCGTAAACCAAAAAAATGGGGC\
 AAATAAGAATTTGATAAGTACCACTTAAATTTA\
 ACTCCTTTGGTTAGAGATGGGCAGCAACTCATT\
+
 CCTGGGACACTCTCAATCATCTATTATTCATATC\
 ATCGTGCTTATACAAGTTAAATCTTAAATCTATA\
 GCACAAATCACATTATCTATTTTGGCAATGATAA\
 TCTCAACCTCACGAGTATGATAGCACAAATCAGT\
 ");
+
+    let mut graph: HashMap<u64, &str> = HashMap::new();
+
     // Cow<str> is a borrow, so it will be &str internally
     let cow_seq: Cow<str> = Cow::Borrowed(&seq);
-    let first_ten = splitter(&cow_seq);
-    println!("{}", first_ten);
+    splitter(&cow_seq, &mut graph);
+    //println!("{}", first_ten);
+    let v = 10u64;
+    println!("{}", graph.get(&v).unwrap());
+
 }
