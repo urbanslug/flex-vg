8ef12cbd119e043ed1c876bc0d986bd16928a1ef
Njagi Mwaniki <njagi@urbanslug.com>
2019-12-17 18:00:44 +0300

    Start on a function to preprocess the VCF and fasta
    Currently gives warnings and panics but would be better if it fixed those errors
    instead. Will figure out how it will fix the errors.

--- src/io/reference.rs
+++ src/io/reference.rs
@@ -26,6 +26,61 @@ fn read_ref() {
     // process_fasta(f);
 }
 
+// Check that the sequences in the reference are in the same order as
+// they are in the VCF
+fn preprocess_vcf_and_fasta<R: Read>(fasta_data: R, vcf_reader: &mut VCFReader<BufReader<R>>) {
+    println!("Preprocessing...");
+
+    // Get the order of chromosomes in the VCF
+    let mut chromosomes_in_vcf: Vec<String> = Vec::new();
+
+    for result_record in vcf_reader.iter() {
+        let record = result_record.unwrap();
+        let chr = record.chromosome;
+        if !chromosomes_in_vcf.contains(&chr) {
+            chromosomes_in_vcf.push(chr);
+        }
+    }
+
+    // Get the order of sequences in the reference
+    let mut sequences_in_fasta: Vec<String> = Vec::new();
+    needletail::parse_sequence_reader(
+        fasta_data,
+        |t| {
+            if t != "FASTA" {
+                panic!("Preprocessing failed: expected a FASTA file but got {}.", t);
+            }
+        },
+        |seq| {
+            let id = std::str::from_utf8(&seq.id).unwrap();
+            sequences_in_fasta.push(String::from(id))
+        },
+    )
+    .expect("Preprocessing the reference failed");
+
+    let mut indices: Vec<usize> = Vec::new();
+    //let mut positions: Vec<usize> = Vec::new();
+    // Check that refererences and variation data are in the same order
+    for chr in chromosomes_in_vcf {
+        // panic if chr not in the reference
+        match sequences_in_fasta.binary_search(&chr) {
+            Ok(index) => indices.push(index),
+            Err(e) => panic!("Couldn't find {} in reference", chr),
+        }
+    }
+
+    let mut sorted_indices = indices.clone();
+    sorted_indices.sort_unstable();
+
+    if indices == sorted_indices {
+        println!("Reference and VCF are in order");
+    } else {
+        println!(
+            "Warning: VCF and refererence aren't in order. Some variation data will be skipped."
+        )
+    }
+}
+
 // File processing
 // TODO: rename gen_graph/build_graph look at libhandlegraph
 fn process_fasta<R: Read>(fasta_data: R, vcf_reader: &mut VCFReader<BufReader<R>>) -> () {
@@ -216,22 +271,75 @@ TCTTGTTCTCAAGACCATGGTGAAATTGCTGAAGCCCTGTGTTGCCTCGC
     }
 
     #[test]
-    fn can_split_ref() {
+    #[ignore]
+    fn test_can_preprocess_input() {
+        let fasta_data = yeild_fasta();
+        let mut vcf_reader = vcf::VCFReader::new(yeild_vcf()).unwrap();
+
+        // TODO: fix panic because of out of order reference and VCF
+        assert_eq!(preprocess_vcf_and_fasta(fasta_data, &mut vcf_reader), ())
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_panic_when_variation_not_in_reference() {
+        let vcf_data: Cursor<&str> = Cursor::new(
+            "
+##fileformat=VCFv4.2
+##FORMAT=<ID=GT,Number=1,Type=Integer,Description=\"Genotype\">
+##FORMAT=<ID=GP,Number=G,Type=Float,Description=\"Genotype Probabilities\">
+##FORMAT=<ID=PL,Number=G,Type=Float,Description=\"Phred-scaled Genotype Likelihoods\">
+#CHROM	POS    	ID    	REF	ALT 	QUAL	FILTER	INFO	FORMAT	SAMP001	SAMP002
+chr1	10	rs21549	T	C	.	PASS	.	GT	0/0	0/1
+chr1	20	rs21549	T	.	.	PASS	.	GT	0/0	0/1
+chr2	33	rs34146	G	C	.	PASS	.	GT	0/0	0/1
+chr2	40	rs44459	G	TA	.	PASS	.	GT	0/0	0/1
+chr3	23	rs71549	A	CA	.	PASS	.	GT	0/0	0/1
+chrX	23	rs71549	A	CA	.	PASS	.	GT	0/0	0/1
+chrY	70	rs91489	A	T,TAC	.	PASS	.	GT	0/0	0/1
+",
+        );
+
+        let mut vcf_reader = vcf::VCFReader::new(vcf_data).unwrap();
+        let fasta_data = yeild_fasta();
+
+        preprocess_vcf_and_fasta(fasta_data, &mut vcf_reader)
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_panic_when_reference_is_fastq() {
+        let mut vcf_reader = vcf::VCFReader::new(yeild_vcf()).unwrap();
+        let fasta_data: Cursor<&str> = Cursor::new(
+            "\
+        @SRR3951347.1.1 1 length=251
+TGTTAGGCTGGTGATTATACATCCCAAGAGGCCCCTTTTCTGCTTT
++
+11>AAFFFFFAFGGGGGGGGGGHA01000000A0ABF1BAD21D11
+@SRR3951347.2.1 2 length=251
+TATCATCAGCGGCTTGCCCGTCTCCGCCCGTAGGGGCCGGGAGATA
++
+>>AABFFFFFBBGGGGGGGGGGC2A22AA2E2221100000001B5
+@SRR3951347.4.1 4 length=251
+TGTCTCACTCATATAGATGCCCACTTCCTATCCCAGACAAAGCAGA
+",
+        );
+
+        preprocess_vcf_and_fasta(fasta_data, &mut vcf_reader)
+    }
+
+    #[test]
+    fn test_can_split_ref() {
         let reference = yeild_fasta();
         let mut vcf_reader = VCFReader::new(yeild_vcf()).unwrap();
 
         process_fasta(reference, &mut vcf_reader);
-        // panic!("Intentional panic for test");
+        // panic!("Intentional panic for print");
     }
 
+    // TODO: Is this test necessary? It tests nothing in *flex-vg* code
     #[test]
-    fn test_fasta_processing() {
-        let data = yeild_fasta();
-        //process_fasta(data);
-    }
-
-    #[test]
-    fn can_read_vcf() {
+    fn test_can_read_vcf() {
         let mut vcf_reader = vcf::VCFReader::new(yeild_vcf()).unwrap();
 
         // VCF Header
