89fcbb6358430938fad7c0b39bb14a0673cfc162
Njagi Mwaniki <njagi@urbanslug.com>
2019-12-17 17:58:58 +0300

    Add a function to generate node hashes
    Given the sequence and an offset we'd like to generate a hash

--- src/graph/utils.rs
+++ src/graph/utils.rs
@@ -1,8 +1,6 @@
-use hex;
 use sha2::{Digest, Sha256};
 use std::array::TryFromSliceError;
 use std::convert::TryInto;
-
 
 // We want to return store hashes as 32 byte arrays instead of
 // hex strings to be conservative with memory usage
@@ -10,10 +8,34 @@ fn sha256(msg: &[u8]) -> Result<[u8; 32], TryFromSliceError> {
     Sha256::digest(msg).as_slice().try_into()
 }
 
+pub fn gen_node_hash(seq: &str, pos: usize) -> Result<[u8; 32], TryFromSliceError> {
+    let mut hasher = Sha256::new();
+    hasher.input(seq.as_bytes());
+
+    // TODO: what if we used to_le/be_bytes here?
+    // Return the memory representation of this integer as a byte array
+    let p = format!("+{}", pos);
+    hasher.input(p.as_bytes());
+
+    hasher.result().as_slice().try_into()
+}
+
 #[cfg(test)]
 mod tests {
 
+    use hex;
+
     use super::*;
+
+    #[test]
+    fn test_gen_correct_node_hash() {
+        let txt = "Hello, world!";
+        let b_txt = txt.to_owned() + "+23";
+        let result_hash = sha256(b_txt.as_bytes()).unwrap();
+
+        let other_hash = gen_node_hash(txt, 23).unwrap();
+        assert_eq!(other_hash, result_hash);
+    }
 
     #[test]
     fn test_gen_correct_hash() {
